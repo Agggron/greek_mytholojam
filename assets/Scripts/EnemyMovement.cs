f93fa311b2a1d9e5e8696bd99c46f562c0f92dc3
Aaron Tang <aaronwt@berkeley.edu>
2017-11-21 01:29:05 -0800

    Added enemy that traverses environment and follows player

---
+++ assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+	Transform player;
+	NavMeshAgent nav;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update()
+	{
+		nav.SetDestination (player.position);
+	}
+}
